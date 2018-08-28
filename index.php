1c3f6c68a9ac26e27839be7c1924f45ac33e63a2
Haerul Muttaqin <haerul.muttaqin@gmail.com>
2018-08-28 22:36:11 +0700

    add a file

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="bm.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+<div class="header">
+        <h1>SISPEK</h1>
+        <p>Ubah Tampilan Browser Untuk Melihat effect.</p>
+      </div>
+               <!-- Top Navigasi -->
+        <ul>
+            <li class="dropdown"><a href="#menu" class="dropbtn">Menu</a>
+            <div class="dropdown-content">
+                <a href="#">Data Penduduk</a>
+                <a href="#">Iuran</a>
+                <a href="#">Kematian</a>
+                <a href="#">Berita</a>
+                <a href="#">Report</a>
+            </div></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+
+            <li class="active">
+                <a  href="#signup">Signup</a>
+                <a  href="#login">Login</a>
+            </li>
+
+        </ul>
+            <!-- <div class="button"><a href="button"></a>
+                <li>
+                        <button class="button button1">Button</button>
+                        <button class="button button1">Button</button>
+                        
+                        <button class="button button1">Button</button>
+                        <button class="button button1">Button</button>
+                        
+                </li>
+
+            </div> -->
+
+        <div class="row">
+
+                <div class="leftcolumn">
+                  <div class="card">
+                   <h2>TITLE HEADING</h2>
+                    <h5>Title description, Dec 7, 2017</h5>
+                    <div class="fakeimg" style="height:200px;">Image</div>
+                    <p>Some text..</p>
+                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                  </div>
+                  
+                  <div class="card">
+                    <h2>TITLE HEADING</h2>
+                    <h5>Title description, Sep 2, 2017</h5>
+                    <div class="fakeimg" style="height:200px;">Image</div>
+                    <p>Some text..</p>
+                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                  </div>
+                </div>
+
+                <!-- <div class="rightcolumn">
+                  <div class="card">
+                    <h2>About Me</h2>
+                    <div class="fakeimg" style="height:100px;">Image</div>
+                    <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                  </div>
+                  <div class="card">
+                    <h3>Popular Post</h3>
+                    <div class="fakeimg"><p>Image</p></div>
+                    <div class="fakeimg"><p>Image</p></div>
+                    <div class="fakeimg"><p>Image</p></div>
+                  </div>
+                  <div class="card">
+                    <h3>Follow Me</h3>
+                    <p>Some text..</p>
+                  </div>
+                </div> -->
+              </div>
+              
+              <div class="footer">
+                <h2>Footer</h2>
+              </div>
+<!-- 
+<ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li class="dropdown">
+          <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+          <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </li>
+      </ul>
+                 -->
+</body>
+</html>

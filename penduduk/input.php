8389e504bbaaa2a67495cda933523bb78444cdc1
Haerul Muttaqin <haerul.muttaqin@gmail.com>
2018-10-04 10:53:39 +0700

    updated

--- penduduk/input.php
+++ penduduk/input.php
@@ -1,157 +1,79 @@
-<script>
-$(document).ready(function(){
-
-	$('#for-load-rayon').load('for_load_rayon.php');
-
-	$("#iBaru").hide();
-	$("#bSimpanBaru").hide();
-	$("#bBatalSimpan").hide();
-	$("#title2").hide();
-	$("#container-atrr").hide();
-
-
-    $("#bBaru").click(function(){
-
-    	$("#form").slideUp();
-    	$("#ft").slideUp();
-        $("#bSimpanBaru").fadeIn();
-        $("#bBatalSimpan").fadeIn();
-        $("#title2").fadeIn(1000);
-        $("#iBaru").fadeIn(2000);
-        $("#container-atrr").show();
-
-
-
-    });
-
-    $("#bBatalSimpan").click(function(){
-
-    	$("#form").slideDown();
-    	$("#ft").slideDown();
-
-
-    	$("#iBaru").hide();
-    	$("#bSimpanBaru").hide();
-    	$("#container-atrr").hide();
-
-		$("#bBatalSimpan").hide();
-
-		$("#sNilai").fadeIn(500);
-        $("#sKeterangan").fadeIn(1000);
-        $("#sFoto").fadeIn(1500);
-        $("#sSimpan").fadeIn(1700);
-        $("#sBatal").fadeIn(2000);
-
-        $("#title2").hide();
-    });
-
-    $("#bSimpanBaru").click(function() {
-
-    	$.ajax({
-    		url: 'spesifikasi_atribut_simpan.php',
-    		type: 'POST',
-    		dataType: 'html',
-    		data: {namaselect: $('#iBaru').val()},
-    	})
-    	.done(function() {
-    		console.log("success");
-    		$("#form").slideDown();
-    		$("#ft").slideDown();
-	    	$("#iBaru").hide();
-	    	$("#iBaru").val();
-	    	$("#bSimpanBaru").hide();
-			$("#bBatalSimpan").hide();
-			$("#sNilai").fadeIn(500);
-	        $("#sKeterangan").fadeIn(1000);
-	        $("#sFoto").fadeIn(1500);
-	        $("#sSimpan").fadeIn(1700);
-	        $("#sBatal").fadeIn(2000);
-	        $("#title2").hide();
-	        $('#for-load-rayon').load('for_load_rayon.php');
-	        $('#selected').hide();
-    	})
-    	.fail(function() {
-    		console.log("error");
-    	})
-    	.always(function() {
-    		console.log("complete");
-    	});
-
-
-    });
-
-});
-
-</script>
-
-
-
-<br><br>
+<div align="right" style="float:right; display:inline-block; margin-right : 50px; margin-top:-90px;" id="floating">
+	<a id="btnCencel" data-ripple class="btn-floating btn-large">
+		<img id="floating" src="../img/ic_close.png" width="15" height="15">
+	</a>
+</div>
 
 <form id="form" method="post" action="simpan.php" enctype="multipart/form-data">
 
-<div class="col-md-8">
-    <div class="panel" style="padding: 20px; margin: 0px auto;" >
+<h3>Tambah Penduduk</h3>
 
-		<h3>Tambah Admin Rayon</h3>
-		<br>
+	<div class="group">
+		<input type="text" name="nik" class="inputs" id="no_induk" required>
+		<span class="bar"></span>
+		<label class="labels">NIK : </label>
+	</div>
 
-		<div class="form-inline">
+	<div class="group">
+		<input type="text" name="nama" class="inputs" required>
+		<span class="bar"></span>
+		<label class="labels">Nama Lengkap :</label>
+	</div>
 
-			<p id="title"><b>Status : <font color="red"> *  </font></b></p>
+	<div class="group">
+		<input type="text" name="nama" class="inputs" required>
+		<span class="bar"></span>
+		<label class="labels">Tempat Lahir :</label>
+	</div>
 
-			<span>
-				<select name="status" id="status" class="form-control" required>
-					<option value="1">Active</option>
-					<option value="0">Non-Active</option>
-				</select>
-			</span>
+	<div class="group">
+		<span style="margin-left:5px; font-size: 12px; color: #03A9F4;">Tanggal Lahir : </span>
+		<input type="date" name="hp" class="inputs" required>
+		<span class="bar"></span>
+	</div>
 
+	<div class="group">
+		<input type="number" name="hp" class="inputs" required>
+		<span class="bar"></span>
+		<label class="labels">Nomor Handphone :</label>
 		</div>
-		<br>
-		<br>
-		<div class="form-inline">
 
-				<p id="title"><b>Unit Rayon : <font color="red"> *  </font></b></p>
-				<span id="for-load-rayon"></span>
-
+	<div class="group">
+		<input type="text" name="sts_perkawinan" class="inputs" required>
+		<span class="bar"></span>
+		<label class="labels">Status Perkawinan :</label>
 		</div>
-			<br>
-			<br>
 
-			<div class="group" style="width: 300px;">
-				<input type="text" name="no_induk" class="inputs" id="no_induk" required>
+	<div class="group">
+		<input type="text" name="sts_dlm_keluarga" class="inputs" required>
 				<span class="bar"></span>
-				<label class="labels">No. Induk : </label>
+		<label class="labels">Status Dalam Keluarga :</label>
 			</div>
 
-			<div class="group" style="width: 100%;">
-
-					<input type="text" name="nama" class="inputs" required>
+	<div class="group">
+		<input type="text" name="no_kk" class="inputs" required>
 					<span class="bar"></span>
-					<label class="labels">Nama Lengkap : <font color="red"> *  </font></label>
+		<label class="labels">Nomor KK / Nama KK :</label>
 			</div>
 
-			<div class="group" style="width: 100%;">
-
-					<input type="text" name="email" class="inputs" required>
+	<div class="group">
+		<input type="text" name="jenis_kelamin" class="inputs" required>
 					<span class="bar"></span>
-					<label class="labels">Email : <font color="red"> *  </font></label>
+		<label class="labels">Jenis Kelamin :</label>
 			</div>
 
-			<div class="group" style="width: 300px;">
 
-					<input type="number" name="hp" class="inputs" required>
+	<div class="group">
+		<input type="text" name="username" class="inputs" required>
 					<span class="bar"></span>
-					<label class="labels">No. Telepon : </label>
+		<label class="labels">Username :</label>
 			</div>
 			
 
-			<div class="group" style="width: 300px;">
-
+	<div class="group">
 					<input type="password" name="password" class="inputs" required>
 					<span class="bar"></span>
-					<label class="labels">Kata Sandi : <font color="red"> *  </font></label>
+		<label class="labels">Password : </label>
 			</div>
 
 			<div class="form-inline" >
@@ -164,47 +86,23 @@ $(document).ready(function(){
 				</div>
 			</div>
 
-		</div>
-		</div>
-
 	</form>
 
-	<br><br>
-
-	</div>
-
-  </div>
-
 <script src="../js/bootstrap.min.js"></script>
- <script src="../js/sidebar.js"></script>
  <script src="../js/jquery-3.2.1.min.js"></script>
-
    <script src="../js/bootstrap.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/ripple.js"></script>
+<script>
 
+$('#btnCencel').click(function () {
 
-<script type="text/javascript">
+	$('#tambah_link').hide();
 
-	Array.prototype.forEach.call(document.querySelectorAll('[data-ripple]'), function(element){
-	  new RippleEffect(element);
-	});
-
-
-	Array.prototype.forEach.call(document.querySelectorAll('[data-ripple-dark]'), function(element){
-	  new RippleEffectDark(element);
-    });
-    
-
-});
-
-$(document).on('click','#backToHome',function () {
-
-$('#view-data').hide();
-
-$('#tabelView').show();
+	if ($('#data').load('tabel.php')) {
+		$('#data').fadeIn();
+	}
 
 });
 
     </script>
-</html>

6ff756fb350c73ad5abbd79691e921b07cbda2f2
arifbm.id <arif.bm91@gmail.com>
2018-10-13 00:12:51 +0700

    testing

--- penduduk/index.php
+++ penduduk/index.php
@@ -9,7 +9,7 @@ $title = "Data Penduduk";
 	<title>SISPEK - <?php echo $title; ?></title>
 <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
 <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="../js/jquery-3.2.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">

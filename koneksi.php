7932e8e305b4e314b931c954f5bb2189a840b5ca
arifbm.id <arif.bm91@gmail.com>
2018-10-19 23:17:25 +0700

    password koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 
 	$servername = "localhost";
 	$username = "root";
-	$password = "root";
+	$password = "kopimanis";
 	$dbname = "sispek";
 
 	$conn = new mysqli($servername, $username, $password, $dbname);
